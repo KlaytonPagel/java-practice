a2e2b3c481e8ba29aa599c1da75063bf7c7a2e74
Klayton Pagel <pagelk@lopers.unk.edu>
2024-01-25 20:40:13 -0600

    Hello World
    Yayyy a hello world program

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,8 @@
+// Main class for all methods
+public class Main {
+
+    // Main method to run code
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
