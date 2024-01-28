7fb75a1459dd0069c7aace627ab2034ca2bebcc5
Klayton Pagel <pagelk@lopers.unk.edu>
2024-01-27 22:02:41 -0600

    Push and Pop
    added a push method for adding items to a stack. added a pop method for removing and returning items from the stack

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Stack stack = new Stack();
-        stack.display();
+        System.out.println(stack.pop());
     }
 }
--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -2,12 +2,29 @@ public class Stack {
     String[] contents = new String[5];
 
     Stack(){
-
     }
 
     public void display() {
         for (String item : contents){
             System.out.println(item);
         }
+    }
+    public void push(String element){
+        for (int index = 0; index < contents.length; index++){
+            if (contents[index] == null){
+                contents[index] = element;
+                break;
+            }
+        }
+    }
+    public String pop() {
+            for (int index = contents.length - 1; index > 0; index--) {
+                if (contents[index] != null) {
+                    String element = contents[index];
+                    contents[index] = null;
+                    return element;
+                }
+            }
+        return null;
     }
 }
