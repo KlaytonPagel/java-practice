f83f703dab011791c85f5260d8bc1ad5824fee7c
Klayton Pagel <pagelk@lopers.unk.edu>
2024-01-27 22:15:48 -0600

    Comments and length
    added comments and a method that returns the length of the stack

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Stack stack = new Stack();
+        stack.push("hello");
+        System.out.println(stack.length());
         System.out.println(stack.pop());
+        System.out.println(stack.length());
     }
 }
--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -4,12 +4,16 @@ public class Stack {
     Stack(){
     }
 
+    // Displays everything currently in the stack_______________________________________________________________________
     public void display() {
+        // iterates through all items in the stack and prints them out
         for (String item : contents){
             System.out.println(item);
         }
     }
+    // adds an item to the stack________________________________________________________________________________________
     public void push(String element){
+        // iterates through the stack to find the next open slot and puts the item their
         for (int index = 0; index < contents.length; index++){
             if (contents[index] == null){
                 contents[index] = element;
@@ -17,14 +21,29 @@ public class Stack {
             }
         }
     }
+    // removes and returns the last item added to the stack_____________________________________________________________
     public String pop() {
-            for (int index = contents.length - 1; index > 0; index--) {
-                if (contents[index] != null) {
-                    String element = contents[index];
-                    contents[index] = null;
-                    return element;
-                }
+        // iterates back words through the stack and returns the last item
+        for (int index = contents.length - 1; index >= 0; index--) {
+            if (contents[index] != null) {
+                String element = contents[index];
+                contents[index] = null;
+                return element;
             }
+        }
         return null;
     }
-}
+
+    // returns the current size of the stack____________________________________________________________________________
+    public int length(){
+        int size = 0;
+        // iterates through each item in the stack counting how many slots are full
+        for (String item : contents){
+            if (item != null){
+                size++;
+            }
+        }
+        return size;
+        }
+    }
+
