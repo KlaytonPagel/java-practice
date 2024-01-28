f3c546d451589b83d8f8c863b678e1ce8b10c439
Klayton Pagel <pagelk@lopers.unk.edu>
2024-01-27 22:31:26 -0600

    Size Increase
    added a method to increase the stacks size when the max size is reached

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,5 +1,6 @@
 public class Stack {
-    String[] contents = new String[5];
+    int maxSize = 5;
+    String[] container = new String[maxSize];
 
     Stack(){
     }
@@ -7,16 +8,20 @@ public class Stack {
     // Displays everything currently in the stack_______________________________________________________________________
     public void display() {
         // iterates through all items in the stack and prints them out
-        for (String item : contents){
-            System.out.println(item);
+        for (String item : container){
+            if (item != null) {
+                System.out.println(item);
+            }
         }
     }
     // adds an item to the stack________________________________________________________________________________________
     public void push(String element){
         // iterates through the stack to find the next open slot and puts the item their
-        for (int index = 0; index < contents.length; index++){
-            if (contents[index] == null){
-                contents[index] = element;
+        for (int index = 0; index < container.length; index++){
+            if (container[index] == null){
+                container[index] = element;
+                // increase the size of the stack if it reaches the max size
+                if (this.length() == maxSize){this.increaseSize();}
                 break;
             }
         }
@@ -24,10 +29,10 @@ public class Stack {
     // removes and returns the last item added to the stack_____________________________________________________________
     public String pop() {
         // iterates back words through the stack and returns the last item
-        for (int index = contents.length - 1; index >= 0; index--) {
-            if (contents[index] != null) {
-                String element = contents[index];
-                contents[index] = null;
+        for (int index = container.length - 1; index >= 0; index--) {
+            if (container[index] != null) {
+                String element = container[index];
+                container[index] = null;
                 return element;
             }
         }
@@ -38,12 +43,21 @@ public class Stack {
     public int length(){
         int size = 0;
         // iterates through each item in the stack counting how many slots are full
-        for (String item : contents){
+        for (String item : container){
             if (item != null){
                 size++;
             }
         }
         return size;
         }
+
+    public void increaseSize(){
+        maxSize += 5;
+        String[] newContainer = new String[maxSize];
+        for (int index = 0; index < container.length; index++){
+            newContainer[index] = container[index];
+        }
+        container = newContainer;
+    }
     }
 
