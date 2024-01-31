58fc01b4431bb69a73bc9630272b59a43210a034
Klayton Pagel <pagelk@lopers.unk.edu>
2024-01-31 00:13:57 -0600

    Password File Checker
    Added a method to the password checker to check the users password against all the passwords in a word list

--- PasswordChecker/src/Main.java
+++ PasswordChecker/src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         PasswordChecker passCheck = new PasswordChecker();
-        String password = "LmA7ZTyx120!@#";
-        System.out.println(password);
-        boolean length = passCheck.checkLength(password);
-        boolean repeat = passCheck.checkRepeat(password);
-        boolean contains = passCheck.checkContains(password);
-        System.out.println("Repeat Test Pass: " + repeat);
-        System.out.println("Length Test Pass: " + length);
-        System.out.println("Contains Test Pass: " + contains);
+        String password = "password";
+        passCheck.checkPassword(password);
+        passCheck.checkFile(password);
     }
 }
--- PasswordChecker/src/PasswordChecker.java
+++ PasswordChecker/src/PasswordChecker.java
@@ -1,9 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 // Class used to check a password for specific requirements_____________________________________________________________
 public class PasswordChecker {
-    String password;
     int minLength;
     int maxTypeRepeat;
-    String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h"};
 
     // Constructor______________________________________________________________________________________________________
     PasswordChecker() {
@@ -12,7 +16,16 @@ public class PasswordChecker {
     }
 
     // Gather user input and check if for all requirements______________________________________________________________
-    public void run() {
+    public void checkPassword(String password) {
+        boolean length = checkLength(password);
+        boolean repeat = checkRepeat(password);
+        boolean contains = checkContains(password);
+
+        if (length && repeat && contains){
+            System.out.println("password is valid");
+        } else {
+            System.out.println("Password does not meet requirements");
+        }
     }
 
     // Method to check if the password meets the length requirement_____________________________________________________
@@ -75,16 +88,37 @@ public class PasswordChecker {
         boolean digit = false;
         boolean letter = false;
         boolean special = false;
+        boolean upper = false;
         for (int index = 0; index < password.length(); index++) {
-            if (Character.isDigit(password.charAt(index))){
+            if (Character.isDigit(password.charAt(index))){ // Checks if the character is a digit
                 digit = true;
-            } else if (Character.isLetter(password.charAt(index))) {
+            } else if (Character.isUpperCase(password.charAt(index))) { // Checks if the character is upper case
+                upper = true;
+            } else if (Character.isLetter(password.charAt(index))){ // checks if the character is a letter
                 letter = true;
-            } else {
+            } else { // if it's not a letter or digit it must be a special character
                 special = true;
             }
         }
-        if (digit && letter && special){return true;}
+        if (digit && letter && special && upper){return true;}
         return false;
+    }
+
+    // Checks if the password is in a file of commonly used passwords___________________________________________________
+    public boolean checkFile(String password){
+        File passFile = new File("passwords.txt");
+        try {
+            Scanner reader = new Scanner(passFile);
+            while (reader.hasNext()){
+                String line = reader.next();
+                if (password.contentEquals(line)){
+                    System.out.println("Password is to common");
+                    return false;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
     }
 }
